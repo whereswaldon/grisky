3c960d8fe19b6e8e0e3b19c47fee49ca87c9f7d4
Christopher Waldon <christopher.waldon.dev@gmail.com>
2017-05-07 19:03:01 -0400

    Refactor main to create board abstraction

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 )
 
 var (
-	colors = [...]string{"red", "orange", "yellow", "green", "pink", "gold", "chocolate"}
+	colors = [...]string{"red", "orange", "yellow", "green", "pink", "gold", "chocolate", "blue", "white", "gray"}
 )
 
 const (
@@ -26,31 +26,48 @@ func getColor() string {
 	return colors[rand.Int31n(int32(len(colors)))]
 }
 
-func main() {
-	rand.Seed(time.Now().Unix())
-	// Create an array to track the distribution of territories between continents
-	numContinents := rand.Int31n(MAX_CONTINENTS-MIN_CONTINENTS) + MIN_CONTINENTS
-	continentCounts := make([]int, numContinents)
-	continentsNeedingAllocation := TOTAL_NODES - numContinents*MIN_TERRITORIES_PER_CONTINENT
+type Board struct {
+	numContinents   int
+	continentCounts []int
+	vizgraph        *gv.Graph
+	solvgraph       *alg.Graph
+}
+
+func (b *Board) allocateContinents() {
+	continentsNeedingAllocation := TOTAL_NODES - b.numContinents*MIN_TERRITORIES_PER_CONTINENT
 	// randomly allocate territories
 	for continentsNeedingAllocation > 0 {
-		continentCounts[rand.Int31n(numContinents)]++
+		b.continentCounts[rand.Int31n(int32(b.numContinents))]++
 		continentsNeedingAllocation--
 	}
 	// ensure each continent has the minimum quantity of territories
-	for i := range continentCounts {
-		continentCounts[i] += MIN_TERRITORIES_PER_CONTINENT
+	for i := range b.continentCounts {
+		b.continentCounts[i] += MIN_TERRITORIES_PER_CONTINENT
 	}
-	fmt.Fprintln(os.Stderr, continentCounts)
+	fmt.Fprintln(os.Stderr, b.continentCounts)
+}
+
+func (b *Board) createGraph() {
 	graph := gv.NewGraph()
 	solver := alg.NewGraph(TOTAL_NODES)
 	for i := 0; i < TOTAL_NODES; i++ {
 		graph.AddNode("G", strconv.Itoa(i), nil)
 	}
-	for _, node := range graph.Nodes.Nodes {
-		node.Attrs.Add("style", "filled")
-		node.Attrs.Add("fillcolor", getColor())
+	currentNode := 0
+	for i := 0; i < b.numContinents; i++ {
+		currentColor := colors[i]
+		for k := 0; k < b.continentCounts[i]; k++ {
+			node := graph.Nodes.Nodes[currentNode]
+			currentNode++
+			node.Attrs.Add("style", "filled")
+			node.Attrs.Add("fillcolor", currentColor)
+		}
 	}
+	b.vizgraph = graph
+	b.solvgraph = solver
+}
+
+func (b *Board) assignEdges() {
 	var other, current int
 	for i := 0; i < MAX_EDGES; i++ {
 		other = int(rand.Int31n(TOTAL_NODES))
@@ -58,11 +75,35 @@ func main() {
 		for other == current {
 			other = int(rand.Int31n(TOTAL_NODES))
 		}
-		graph.AddEdge(strconv.Itoa(current),
+		b.vizgraph.AddEdge(strconv.Itoa(current),
 			strconv.Itoa(other), false, nil)
-		solver.InsertEdge(current, other, 1)
+		b.solvgraph.InsertEdge(current, other, 1)
 	}
-	fmt.Println(graph)
-	s, e, d := solver.FindDiameter()
-	fmt.Fprintf(os.Stderr, "Diameter: %d\nStart: %d\nEnd: %d\n", d, s, e)
+}
+func MakeBoard(size int) *Board {
+	// Create an array to track the distribution of territories between continents
+	numContinents := rand.Int31n(MAX_CONTINENTS-MIN_CONTINENTS) + MIN_CONTINENTS
+	continentCounts := make([]int, numContinents)
+	board := &Board{
+		numContinents:   int(numContinents),
+		continentCounts: continentCounts,
+	}
+	board.allocateContinents()
+	board.createGraph()
+	board.assignEdges()
+	return board
+}
+
+func (b *Board) String() string {
+	return b.vizgraph.String()
+}
+
+func main() {
+	rand.Seed(time.Now().Unix())
+	board := MakeBoard(TOTAL_NODES)
+	fmt.Println(board)
+	/*
+		s, e, d := solver.FindDiameter()
+		fmt.Fprintf(os.Stderr, "Diameter: %d\nStart: %d\nEnd: %d\n", d, s, e)
+	*/
 }
